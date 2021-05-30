5cad89ed44f3fb7e03022ee9a7624b64243b3857
nao18 <justdo.nao01@gmail.com>
2021-05-30 22:57:35 +0900

    3桁区切り追加

--- lib/logic.dart
+++ lib/logic.dart
@@ -1,9 +1,13 @@
+import 'package:intl/intl.dart' as intl;
+
 class Logic {
   late String _text = "0";
   get text => _text;
 
   // 現在の値
   double _currentValue = 0;
+
+  intl.NumberFormat formatter = intl.NumberFormat('#,###', 'es_US');
 
   void input(String text) {
     if (_text == "0") {
@@ -12,6 +16,6 @@ class Logic {
       _currentValue = _currentValue * 10 + double.parse(text);
     }
 
-    _text = _currentValue.toStringAsFixed(0);
+    _text = formatter.format(_currentValue);
   }
 }
--- test/logic_test.dart
+++ test/logic_test.dart
@@ -26,5 +26,19 @@ void main() {
     expect(logic.text, "1");
     logic.input("2");
     expect(logic.text, "12");
+    logic.input("3");
+    expect(logic.text, "123");
+    logic.input("4");
+    expect(logic.text, "1,234");
+    logic.input("5");
+    expect(logic.text, "12,345");
+    logic.input("6");
+    expect(logic.text, "123,456");
+    logic.input("7");
+    expect(logic.text, "1,234,567");
+    logic.input("8");
+    expect(logic.text, "12,345,678");
+    logic.input("9");
+    expect(logic.text, "123,456,789");
   });
 }
